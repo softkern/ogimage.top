cb1772057afe1d67c011dfd05f677332e70742f9
softkern <hi@softkern.com>
2025-01-05 00:06:22 +0800

    add umami components

---
+++ app/Umami.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Script from "next/script";
+
+const Umami = () => {
+  return (
+    <>
+      {process.env.NEXT_PUBLIC_UMAMI_URL && process.env.NEXT_PUBLIC_UMAMI_ID ? (
+        <>
+          <Script
+            async
+            src={process.env.NEXT_PUBLIC_UMAMI_URL}
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+          />
+        </>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default Umami;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import "@/styles/globals.css"
 
-import type { Metadata } from "next"
 import { TemplateStoreProvider } from "@/providers/template-store-provider"
 import { GeistMono } from "geist/font/mono"
 import { GeistSans } from "geist/font/sans"
+import type { Metadata } from "next"
 
-import { siteConfig } from "@/config/site"
-import { Separator } from "@/components/ui/separator"
-import { Toaster } from "@/components/ui/sonner"
-import Footer from "@/components/footer/Footer"
-import { ThemeProvider } from "@/components/theme-provider"
 import GoogleAdsense from "@/app/GoogleAdsense"
 import GoogleAnalytics from "@/app/GoogleAnalytics"
+import Umami from "@/app/Umami"
+import Footer from "@/components/footer/Footer"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Separator } from "@/components/ui/separator"
+import { Toaster } from "@/components/ui/sonner"
+import { siteConfig } from "@/config/site"
 
 export const metadata: Metadata = {
   ...siteConfig,
@@ -48,8 +49,7 @@ export default async function RootLayout({
           <>
             <GoogleAnalytics />
             <GoogleAdsense />
-            {/* <SpeedInsights />
-            <Analytics /> */}
+            <Umami />
           </>
         ) : (
           <></>
